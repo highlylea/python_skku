185696f4936f5a064be62ff9eb392e15ffcf6ef2
highlylea <35249399+highlylea@users.noreply.github.com>
2020-08-07 03:25:06 +0900

    성균공부방 1강 과제
    1. 섭씨를 화씨로 변환합니다.

---
+++ 1st/c_to_f.py
@@ -0,0 +1,6 @@
+#1차시도
+#temperature=float(input("This program converts Celsius to Fahrenheit. Celsius:"))
+#print("It is", temperature*9/5+32, "in fahrenheit.")
+
+#2차시도
+print('It is {}°F.'.format(float(input('This program converts Celsius to Fahrenheit. Celsius:'))*9/5+32))
