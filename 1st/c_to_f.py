374871d673593ff7a0fc1e58d9a70ccc36aa3752
highlylea <35249399+highlylea@users.noreply.github.com>
2020-08-07 04:44:49 +0900

    성균공부방 파이썬 1강 과제
    파이썬 2강에서 함수를 학습한 뒤, 1강 온도변환기 과제에 적용해 재업로드합니다. :)

--- 1st/c_to_f.py
+++ 1st/c_to_f.py
@@ -1,6 +1,20 @@
-#1차시도
-#temperature=float(input("This program converts Celsius to Fahrenheit. Celsius:"))
-#print("It is", temperature*9/5+32, "in fahrenheit.")
+"""
+1차시도_두줄연습
+temperature=float(input("This program converts Celsius to Fahrenheit. Celsius:"))
+print("It is", temperature*9/5+32, "in fahrenheit.")
 
-#2차시도
+2차시도_한줄연습
 print('It is {}°F.'.format(float(input('This program converts Celsius to Fahrenheit. Celsius:'))*9/5+32))
+
+
+#3차시도_함수연습_
+def ctof():
+    c = float(input("섭씨 온도를 입력하세요.:"))
+    return c*9/5+32  
+print("화씨", ctof(),"도 입니다.")
+
+""" 
+#4차시도_함수 더 짧게
+def ctof():
+    return float(input("섭씨 온도를 입력하세요:"))*9/5+32
+print(ctof(),"도 입니다.")
